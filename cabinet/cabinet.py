e415c2d85c94c60639bf5dfe3baf2415c96ac1d0
Bo Kamphues <b.kamphues@outlook.com>
2020-11-07 14:49:56 +0100

    Added pylint and setup virtualenv

--- cabinet/cabinet.py
+++ cabinet/cabinet.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from gui import MainWindow
 
 def main():
-    print("Hello world!")
+    
+    window = MainWindow()
